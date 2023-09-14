93906fc3bb5333680fbc06a00127c75b6b93d0eb
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-09-14 19:33:35 +0530

    push

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public readonly IMongoCollection<Order> _Orders;
+        public OrderController(IMongoClient mongoClient)
+        {
+            var Database = mongoClient.GetDatabase("pregopantry");
+            _Orders = Database.GetCollection<Order>("Orders");
+        }
+
+        [HttpGet("GetOrders")]
+        public IActionResult GetOrders(string jwt)
+        {
+            var order = _Orders.Find( c => c.Jwt == jwt).ToList();
+            if(order != null)
+            {
+                return Ok(order);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("CreateOrder")]
+        public IActionResult CreateOrder(Order order)
+        {
+            _Orders.InsertOne(order);
+            return Ok(order);
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace API.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Order
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("jwt")]
+        public string Jwt { get; set; }
+
+        [BsonElement("Ordername")]
+        public string OrderName { get; set; }
+
+        [BsonElement("FoodName")]
+        public string FoodName { get; set; }
+
+        [BsonElement("totalPrice")]
+        public string TotlaPrice { get; set; }
+    }
+}
